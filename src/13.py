3486b4cfd4e8c860472b252509979dbb493c9a7d
elickkozicki806 <elickkozicki806@gmail.com>
2025-03-17 17:54:32 +0000

    Add 13.py

---
+++ src/13.py
@@ -0,0 +1,11 @@
+def random_python_code():
+    import random
+
+    # Generate a random number between 1 and 10
+    num = random.randint(1, 10)
+
+    # Print the number to the console
+    print(num)
+
+# Call the function
+random_python_code()
