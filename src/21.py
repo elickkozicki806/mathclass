bb94317b4368cd97e1153855a21318fdc92b9334
elickkozicki806 <elickkozicki806@gmail.com>
2025-03-29 18:49:33 +0000

    Add 21.py

---
+++ src/21.py
@@ -0,0 +1,13 @@
+def calculate_square_root(num):
+    """
+    Calculate and return the square root of a given number.
+    
+    Parameters:
+    num (float): The number to calculate the square root of.
+    
+    Returns:
+    float: The square root of the input number.
+    """
+    return num ** 0.5
+
+print(calculate_square_root(16))  # Output: 4
